c2552041e8005555fcaf14e3a0eb6b320331e328
Ishita Kalra <ishitakalra1711@gmail.com>
2025-02-24 01:59:44 +0530

    Solution to Perform Bitwise One's Complement

---
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a, i, isPrime = 1; // Assume number is prime
+    scanf("%d", &a);
+
+    if (a <= 1) {
+        printf("Not Prime");
+        return 0;
+    }
+
+    for (i = 2; i * i <= a; i++) {
+        if (a % i == 0) {
+            isPrime = 0; // Found a divisor, not prime
+            break;
+        }
+    }
+
+    if (isPrime)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
