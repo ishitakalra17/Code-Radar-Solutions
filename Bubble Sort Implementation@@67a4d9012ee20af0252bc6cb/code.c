198b9a12938f88dda62fe99dbf590e84fc6f98a2
Ishita Kalra <ishitakalra1711@gmail.com>
2025-03-29 08:04:23 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,65 +1,10 @@
-#include <stdio.h>
-
-// Function to perform Bubble Sort
-void bubbleSort(int arr[], int n) {
-    int i, j, temp;
-    int swapped;
-
-    for (i = 0; i < n - 1; i++) {
-        swapped = 0; // Flag to optimize sorting
-
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = 1;
-            }
-        }
-
-        // If no swapping occurred, the array is already sorted
-        if (swapped == 0) {
-            break;
-        }
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%D",&n);
+    for(int i=0;i<a;i++)
+    {scnaf("%d",&i);
     }
-}
-
-// Function to print an array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-// Main function
-int main() {
-    int arr[6], n, i;
-
-    // Take user input for the array size (5 or 6 elements)
-    printf("Enter the number of elements (5 or 6): ");
-    scanf("%d", &n);
-
-    if (n < 5 || n > 6) {
-        printf("Please enter only 5 or 6 elements.\n");
-        return 1; // Exit program if invalid input
-    }
-
-    // Take user input for array elements
-    printf("Enter %d elements:\n", n);
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Original array: \n");
-    printArray(arr, n);
-
-    // Sort the array using Bubble Sort
-    bubbleSort(arr, n);
-
-    printf("Sorted array: \n");
-    printArray(arr, n);
-
     return 0;
 }
