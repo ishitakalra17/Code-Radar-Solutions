ffa794f08195567f9c7eee397901ef446e9f0b2e
Ishita Kalra <ishitakalra1711@gmail.com>
2025-03-31 22:36:52 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-int fibonacci(int a)
-{
-    if (a == 0)
-        return 0;
-    if (a == 1)
-        return 1;
-    
-    return fibonacci(a - 1) + fibonacci(a - 2);  // Recursive call
+int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    int a = 0, b = 1, temp;
+    for (int i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 }
 
 int main()
 {
     int num;
-    printf("Enter a number: ");  // Some platforms require an explicit prompt
-    scanf("%d", &num);  // Read user input
-
-    int result = fibonacci(num);  // Call the Fibonacci function
-    printf("Fibonacci of %d is: %d\n", num, result);  // Print properly formatted output
-
+    scanf("%d", &num);
+    printf("%d\n", fibonacci(num));  // Ensure newline for platform requirements
     return 0;
 }
+
+
