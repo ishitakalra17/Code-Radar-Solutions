db89305a481b73417bc70dfeedaddc4b0536367d
Ishita Kalra <ishitakalra1711@gmail.com>
2025-03-31 21:53:51 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int fibonacci()
+{
+    int a;
+    scanf("%d",&a);
+    if(n==0)
+    return 0;
+    if(n==1)
+    return 1;
+    else(return fibonacci(a-1)+fibonacci(a-2);)
+    return 0;
+}
